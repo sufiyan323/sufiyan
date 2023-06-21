5307592c0c6c530e34fbdce911dc3acc288ff326
sufiyan323 <137264202+sufiyan323@users.noreply.github.com>
2023-06-21 12:23:10 +0530

    Add files via upload

---
+++ 2darray.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print("\n the first matrices is:")
+A=np.array([[1,2,3],[4,5,6]])
+print(A)
+print("\n the second matrices is:")
+B=np.array([[1,2,3],[4,5,6]])
+print(B)
+print("\n the sum of two matrices are:")
+print(np.add(A,B))
+print("\n the multiplication of two matrices are:")
+print(np.multiply(A,B))
